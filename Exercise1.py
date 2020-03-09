4e2545e963f2b3e84496946a463b466fbde87f29
MichaelBruwer <61545579+MichaelBruwer@users.noreply.github.com>
2020-03-09 11:37:19 +0200

    Add files via upload
    Added Exercise 2

--- Exercise1.py
+++ Exercise1.py
@@ -5,3 +5,16 @@ pi = 3.24
 A = pi * (r ** 2)
 #Displaying answer
 print (A)
+
+#Declareing varibles
+a=4
+b=8
+c=4
+d=10
+#Calculations
+num= (a-3)**1/2
+denom= b**2+c**2+d**2
+#Defineing X
+x=num/denom
+#Displating answer
+print(x)
