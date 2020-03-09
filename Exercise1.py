c26ecd78372cd43cd3d9659d48360bc9e78aeb04
MichaelBruwer <61545579+MichaelBruwer@users.noreply.github.com>
2020-03-09 11:44:57 +0200

    Add files via upload

--- Exercise1.py
+++ Exercise1.py
@@ -18,3 +18,13 @@ denom= b**2+c**2+d**2
 x=num/denom
 #Displating answer
 print(x)
+
+#Varibles
+AA=4
+BB=8
+CC=4
+DD=10
+#calculations
+Version_2 = (AA-3)**(1/2)/(BB**2 + CC**2 + DD**2)
+#Displaying answer
+print(Version_2)
